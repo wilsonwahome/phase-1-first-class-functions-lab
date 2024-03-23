417998ab49a8abc8e1c9eac46607b165b2a08fcb
wahome <wahomewgitonga@gmail.com>
2024-03-23 16:43:09 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+
+  function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+  }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+ const createFareMultiplier = function(multiplier) {
+    return function(value) {
+      return value * multiplier;
+    };
+  }; 
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectFunction) {
+  return selectFunction(drivers);
+}
